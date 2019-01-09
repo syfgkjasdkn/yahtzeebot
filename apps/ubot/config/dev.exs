859135eededcf347a01ce45108cd3965f7ba1c5e
adsfiuqweliuf <asdiufahsd@aksjdfh>
2019-01-09 15:56:13 +0000

    set tdlib db dir in dev

--- apps/ubot/config/dev.exs
+++ apps/ubot/config/dev.exs
@@ -4,4 +4,5 @@ config :ubot,
   api_id: System.get_env("TG_API_ID") || raise("need TG_API_ID"),
   api_hash: System.get_env("TG_API_HASH") || raise("need TG_API_HASH"),
   phone_number: System.get_env("TG_PHONE_NUMBER") || raise("need TG_PHONE_NUMBER"),
-  start_ubot?: true
+  start_ubot?: true,
+  tdlib_database_directory: "/tmp/tdlib"
