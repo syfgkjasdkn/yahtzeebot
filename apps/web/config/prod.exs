5fb3958817414664ae7328ef332930e7755fd9c3
adsfiuqweliuf <asdiufahsd@aksjdfh>
2019-01-09 15:34:12 +0000

    simplify web config

--- apps/web/config/prod.exs
+++ apps/web/config/prod.exs
@@ -2,6 +2,8 @@ use Mix.Config
 
 # in prod we load config from system env (see rel/etc/config.exs)
 config :web,
+  generate_certs?: true,
+  set_webhook?: true,
   options: [
     otp_app: :web,
     keyfile: "priv/server.key",
